a974254fa90a8e0f1a2522c4bce7573eb5c40221
ddonggsa <qoguswl605@naver.com>
2021-09-01 14:48:08 +0900

    py01

---
+++ 2021.09.01.py
@@ -0,0 +1,13 @@
+#심사문제 9.4 p113
+s = "\'Python\' is a \"programming language\"\nthat lets you work quickly\nand\nintegrate systems more effectively."
+print(s)
+#리스트(list)->대괄호로 묶어줌 ->[]
+#튜플(tuple)->읽기전용 리스트, 괄호로 묶어줌 ->()
+
+#심사문제 10.5 p123
+x = int(input())
+a = tuple(range(-10,10,x))
+print(a)
+
+
+
